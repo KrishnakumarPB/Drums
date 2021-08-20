5470e711e0cd6f281e3a39208f74709f4dca4c5e
Krishnakumar <krishnakumar.amritham@gmail.com>
2021-08-20 15:28:04 +0530

    UI Changes

--- src/Component.js
+++ src/Component.js
@@ -3,7 +3,7 @@ import Hihat from "./Assets/hihat.mp3"
 import Tom from "./Assets/tom.mp3"
 import Sidestick from  "./Assets/side-stick.mp3"
 import Bell from "./Assets/ride-bell.mp3"
-const Elements=[{id:"Cymbal",audio:Cymbal},{id:"Box",audio:"https://www.myinstants.com/media/sounds/snare.mp3"},,
+const Elements=[{id:"Cymbal",audio:Cymbal},{id:"Box",audio:"https://www.myinstants.com/media/sounds/snare.mp3"},
 {id:"Hitat",audio:Hihat},
 {id:"Tom",audio:Tom},
 {id:"Side Stick",audio:Sidestick},
--- src/Container.js
+++ src/Container.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./style.css"
-let src=""
 let x=""
 function Container(props)
 {
